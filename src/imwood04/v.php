981f29763d0e6f1355c06fca21a0b0adc36f42b8
imwood04 <41460038+imwood04@users.noreply.github.com>
2020-02-03 06:47:27 -0500

    Update v.php

--- src/imwood04/v.php
+++ src/imwood04/v.php
@@ -3,7 +3,8 @@ namespace WoodDevz/Imwood04;
 
 use pocketmine/command/Command;
 use pocketmine/command/CommandSender;
-use pocketmine/event/player/PlayerJoinEvent:
+use pocketmine/event/player/PlayerJoinEvent;
+//im stuck below im trying to use that one for chat but idk what 1 to use
 use pocketmine/
 
 
