812d0aa15842e607911d86d1ef4e711a9f9cd4eb
imwood04 <41460038+imwood04@users.noreply.github.com>
2020-02-06 03:34:02 -0500

    Update v.php

--- src/imwood04/v.php
+++ src/imwood04/v.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace imwood04;
 
 use pocketmine\command\Command;
@@ -7,6 +9,8 @@ use pocketmine\command\CommandSender;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 
 class v implements Listener {
   //put whatever code you want here. not sure what you are trying to do.
